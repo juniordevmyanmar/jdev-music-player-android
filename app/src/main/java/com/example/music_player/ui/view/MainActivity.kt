83cb902bcaa3e57ab8cc2f9cddd5820dff2e627f
DaisyHxx <hsuyee.hyh.01@gmail.com>
2023-04-20 12:31:04 +0630

    first commit

--- app/src/main/java/com/example/music_player/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/music_player/ui/view/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.music_player.ui.view
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
@@ -27,21 +32,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         val loginBtn = findViewById<Button>(R.id.login_btn)
+        val signUpBtn = findViewById<Button>(R.id.signup_btn)
+
         loginBtn.setOnClickListener{
+            signUpBtn.isSelected = false
+            loginBtn.isSelected = !signUpBtn.isSelected
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 replace(R.id.fragment_container_view, LoginFragment())
             }
-//            Toast.makeText("MainActivity","Login Fragment",Toast.SHORT_LEN)
         }
 
-        val signUpBtn = findViewById<Button>(R.id.signup_btn)
+
         signUpBtn.setOnClickListener{
+            loginBtn.isSelected = false
+            signUpBtn.isSelected= !loginBtn.isSelected
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 replace(R.id.fragment_container_view, SignUpFragment())
             }
         }
+
 
 
     }
